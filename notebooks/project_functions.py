f8e4aba8a8a1b0309b9e9397f1e646ecd17b19fd
ChrisPolacco <113083382+ChrisPolacco@users.noreply.github.com>
2022-11-07 04:38:00 -0800

    changes

--- notebooks/project_functions.py
+++ notebooks/project_functions.py
@@ -4,4 +4,5 @@ def load_and_process(raw):
     df = pd.read_csv(raw)
     clean = df.copy().drop(['Remove', 'Source.Name','SourceFile', 'Other', 'Std Dev', 'Audit'], axis=1).drop(clean[clean.Campus == 'Campus'].index).astype(str)
     
+    
     return clean
