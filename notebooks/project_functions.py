1a0e3b9a9ab99b28e5431ef1a1160ff750841310
ChrisPolacco <113083382+ChrisPolacco@users.noreply.github.com>
2022-11-07 23:58:16 -0800

    changed code

--- notebooks/project_functions.py
+++ notebooks/project_functions.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 def load_and_process(raw):
     df = pd.read_csv(raw)
-    clean = (df.copy().drop(['Remove', 'Source.Name','SourceFile', 'Other', 'Std Dev', 'Audit'], axis=1)
-             .drop(clean[clean.Campus == 'Campus'].index)
+    
+    clean = (df.copy()
+             .drop(['Remove', 'Source.Name','SourceFile', 'Other', 'Std Dev', 'Audit'], axis=1)
              .astype(str)
     )
-    
+    clean = clean.drop(clean[clean.Campus == 'Campus'].index)
+
     
     return clean
